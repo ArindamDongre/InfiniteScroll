720b328be6936d32e421e0457f5912291fe0ba84
Arindam Dongre <84098267+ArindamDongre@users.noreply.github.com>
2024-10-23 19:31:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Infinite Scroller</h1>
+      <h1>Infinity Scroller</h1>
       <InfiniteScroller />
     </div>
   );
